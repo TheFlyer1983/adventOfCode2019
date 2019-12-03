ba27be52c72166662c26217f761a6c67c48224cc
Paul Livingstone <paul.livingstone@aquarium-software.com>
2019-12-03 15:38:03 +0000

    Day Two Part 2,  added Promise to reuse part 1

--- Day 02/script.js
+++ Day 02/script.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 
-function dayTwoPartOne(){
-  console.log('Day 2 Part 1');
-  fs.readFile('./input.txt', (err, data) => {
-    if (err){
-      console.log("You have an error! Sorry");
-    }
+function loadFile(){
+  return new Promise((resolve, reject) => {
+    fs.readFile('./input.txt', (err, data) => {
+      err ? reject(err) : resolve(data);
+    });
+  });
+}
+
+function dayTwoPartOne(data, replacement1, replacement2){
+
     const intCode = data.toString();
     const intCodeArray = intCode.split(',').map(Number);
-    intCodeArray.splice(1, 1, 12);
-    intCodeArray.splice(2, 1, 2);
+    intCodeArray.splice(1, 1, replacement1);
+    intCodeArray.splice(2, 1, replacement2);
 
     let valueOne = 0;
     let valueTwo = 0;
@@ -28,9 +32,28 @@ function dayTwoPartOne(){
       }
       intCodeArray.splice(intCodeArray[i+3], 1, valueToReplace);
     }
-    
-    console.log('Answer', intCodeArray[0]);
-  })
+    const answer = intCodeArray[0];
+
+    return answer;
 }
 
-dayTwoPartOne();
+function dayTwoPartTwo(data){
+  let answer = 0;
+
+  for (let noun = 0; noun <= 99; noun++) {
+    for (let verb = 0; verb <= 99; verb++) {
+      const result = dayTwoPartOne(data, noun, verb);
+      if (result === 19690720) {
+        answer = 100 * noun + verb;
+        return answer;
+      }
+    }
+  }
+}
+
+
+loadFile()
+  .then((data) => {
+    console.log('Day Two Part One Answer:', dayTwoPartOne(data, 12,2));
+    console.log('Day Two Part Two Answer:', dayTwoPartTwo(data));
+  });
